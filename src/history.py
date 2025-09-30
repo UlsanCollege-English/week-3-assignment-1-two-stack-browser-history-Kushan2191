51a1372804472a93c3d626be15b80b4a57329519
Kushan Sunuwar <kushan9826878937@gmail.com>
2025-09-30 09:40:29 +0900

    kushan

--- src/history.py
+++ src/history.py
@@ -1,24 +1,34 @@
-
-### /src/history.py (starter)
+from typing import List
 
 class BrowserHistory:
-    def __init__(self, start="home"):
-        # TODO: choose your internal representation (two stacks)
+    def __init__(self, start: str = "home"):
         self._cur = start
-        self._back = []   # TODO
-        self._fwd = []    # TODO
+        self._back: List[str] = []
+        self._fwd: List[str] = []
+        self._start = start
 
     def visit(self, url: str) -> None:
-        # TODO: push current to back, set current, clear forward
-        raise NotImplementedError
+        if url == self._cur:
+            return
+        # only push to back if current is not the initial start page
+        if self._cur != self._start:
+            self._back.append(self._cur)
+        self._cur = url
+        self._fwd.clear()
 
     def back(self) -> str:
-        # TODO: move to previous page; decide error behavior on underflow
-        raise NotImplementedError
+        if not self._back:
+            raise IndexError("No page to go back to")
+        self._fwd.append(self._cur)
+        self._cur = self._back.pop()
+        return self._cur
 
     def forward(self) -> str:
-        # TODO: move to next page; decide error behavior on underflow
-        raise NotImplementedError
+        if not self._fwd:
+            raise IndexError("No page to go forward to")
+        self._back.append(self._cur)
+        self._cur = self._fwd.pop()
+        return self._cur
 
     def current(self) -> str:
         return self._cur
